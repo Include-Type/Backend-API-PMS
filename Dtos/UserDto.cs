271af38ea2f9a95beed8066877b36c43e1294cc0
Subham Karmakar <subhamk.subs@gmail.com>
2021-07-01 20:10:38 +0530

    Added the User Model and Dto

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace IncludeTypeBackend.Dtos
+{
+    public class UserDto
+    {
+        public string Key { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IncludeTypeBackend.Models
+{
+    public class User
+    {
+        [Key]
+        [MaxLength(100)]
+        public string UserId { get; set; }
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [MaxLength(100)]
+        public string Username { get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; }
+        [MaxLength(100)]
+        public string Password { get; set; }
+    }
+}
