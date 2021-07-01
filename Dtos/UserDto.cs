9cfdba2ac53f9b02e6bf3115d5d7e455c3799a6f
Subham Karmakar <subhamk.subs@gmail.com>
2021-07-01 21:45:01 +0530

    Fixed the Not Nullable bug in the UserId Property

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -1,8 +1,12 @@
-﻿namespace IncludeTypeBackend.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IncludeTypeBackend.Dtos
 {
     public class UserDto
     {
+        [Required]
         public string Key { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -8,14 +8,19 @@ namespace IncludeTypeBackend.Models
         [MaxLength(100)]
         public string UserId { get; set; }
         [MaxLength(100)]
+        [Required]
         public string FirstName { get; set; }
         [MaxLength(100)]
+        [Required]
         public string LastName { get; set; }
         [MaxLength(100)]
+        [Required]
         public string Username { get; set; }
         [MaxLength(100)]
+        [Required]
         public string Email { get; set; }
         [MaxLength(100)]
+        [Required]
         public string Password { get; set; }
     }
 }
