52bfc235178dd15ba0ee2eaf8364bfa3683c1fef
Debayan De <78524177+TheXorcist@users.noreply.github.com>
2021-11-24 20:48:24 +0530

    @ api's added (#4)

--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -14,43 +14,19 @@ public class ProjectService
         return projectTasks.Count;
     }
 
-    public async Task<List<ProjectTask>> GetAllTasksByUsernameAsync(string username)
+    public async Task<List<ProjectTask>> GetAllTasksByUsernameAsync(string username) 
     {
-        /*
-        Method description:
-            Returns: A list of all tasks where `username` is present 
-            in the `Assigned` property of a ProjectTask.
-        
-        Eg:
-            There's a task T1 where T1.Assigned = "rohanhalder + subhamk108"
-            So if username = "subhamk108" is passed to this method as parameter, 
-            it should return a list having T1 and all such tasks.
-
-        Info:
-            `_db` is the DB.
-            All tasks in the DB are accessed by the `_db.ProjectTask` property.
-        */
-        // Implement this method.
-        return await GetAllTasksAsync(); // Delete this temporary line first
+        return await _db.ProjectTask.Where(task => task.Assigned.Contains(username)).ToListAsync();
     }
 
     public async Task UpdateAllTasksAsync(ProjectTask[] projectTasks, string username)
     {
-        /*
-        Method description:
-        Returns: Void / Nothing
-        This method will update all tasks based on the `username`
-
-        Algorithm:
-            - Delete all tasks in the DB whose `Assigned` property contains `username`.
-            - Add all tasks from the `projectTasks` (array) method parameter to the DB.
-            - Save the DB using the `SaveChangesAsync()` function.
-
-        Info:
-            `_db` is the DB.
-            All tasks in the DB are accessed by the `_db.ProjectTask` property.
-        */
-        // Implement this method.
+        List<ProjectTask> ptask = await GetAllTasksByUsernameAsync(username);
+        foreach(ProjectTask p in ptask)
+        {
+            _db.ProjectTask.Remove(p);
+        }
+        await _db.ProjectTask.AddRangeAsync(projectTasks);
         await _db.SaveChangesAsync(); // Delete this temporary line first
     }
 }
