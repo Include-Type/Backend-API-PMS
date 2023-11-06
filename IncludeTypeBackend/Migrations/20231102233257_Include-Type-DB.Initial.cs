73debd9918d1afbf04d06b42ce0fab660aa03d97
SubhamK108 <subhamk.subs@gmail.com>
2023-11-07 03:56:36 +0530

    Restructured the project: Separated the .sln file from the source.

---
+++ IncludeTypeBackend/Migrations/20231102233257_Include-Type-DB.Initial.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IncludeTypeBackend.Migrations
+{
+    public partial class IncludeTypeDBInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "Privacy",
+                schema: "public",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Bio = table.Column<string>(type: "text", nullable: true),
+                    Picture = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Contact = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Education = table.Column<string>(type: "text", nullable: true),
+                    Companies = table.Column<string>(type: "text", nullable: true),
+                    Skills = table.Column<string>(type: "text", nullable: true),
+                    Experience = table.Column<string>(type: "text", nullable: true),
+                    Projects = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Privacy", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProfessionalProfile",
+                schema: "public",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Education = table.Column<string>(type: "text", nullable: true),
+                    Companies = table.Column<string>(type: "text", nullable: true),
+                    Skills = table.Column<string>(type: "text", nullable: true),
+                    ExperienceYears = table.Column<int>(type: "integer", nullable: false),
+                    ExperienceMonths = table.Column<int>(type: "integer", nullable: false),
+                    Projects = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfessionalProfile", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: true),
+                    About = table.Column<string>(type: "text", nullable: true),
+                    Documentation = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectIssue",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ProjId = table.Column<string>(type: "text", nullable: true),
+                    ProjName = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<string>(type: "text", nullable: true),
+                    Details = table.Column<string>(type: "text", nullable: true),
+                    Deadline = table.Column<string>(type: "text", nullable: true),
+                    Assigned = table.Column<string>(type: "text", nullable: true),
+                    Completed = table.Column<bool>(type: "boolean", nullable: false),
+                    Priority = table.Column<string>(type: "text", nullable: true),
+                    Author = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectIssue", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectMember",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ProjName = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Role = table.Column<string>(type: "text", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectMember", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectTask",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ProjId = table.Column<string>(type: "text", nullable: true),
+                    ProjName = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<string>(type: "text", nullable: true),
+                    Details = table.Column<string>(type: "text", nullable: true),
+                    Deadline = table.Column<string>(type: "text", nullable: true),
+                    Assigned = table.Column<string>(type: "text", nullable: true),
+                    Completed = table.Column<bool>(type: "boolean", nullable: false),
+                    Priority = table.Column<string>(type: "text", nullable: true),
+                    Author = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectTask", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    Bio = table.Column<string>(type: "text", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Country = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    State = table.Column<string>(type: "text", nullable: true),
+                    Pincode = table.Column<string>(type: "text", nullable: true),
+                    Contact = table.Column<string>(type: "text", nullable: true),
+                    Picture = table.Column<string>(type: "text", nullable: true),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserVerification",
+                schema: "public",
+                columns: table => new
+                {
+                    UniqueString = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true),
+                    CreationTime = table.Column<string>(type: "text", nullable: true),
+                    ExpirationTime = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserVerification", x => x.UniqueString);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "public",
+                table: "User",
+                columns: new[] { "Id", "Address", "Bio", "City", "Contact", "Country", "Email", "FirstName", "IsAdmin", "LastName", "Password", "Picture", "Pincode", "State", "Username" },
+                values: new object[] { "596398ed-b86b-40eb-a4f9-66675d232440", "", "", "", "", "", "subhamkarmakar0901@gmail.com", "Subham", true, "Karmakar", "$2a$11$BzRSmI/Z8W4ccYj7f.trLuv9EhjAkNbZ8j2QpKrLJVFOnHu.3VeKi", "", "", "", "SubhamK108" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_Name",
+                schema: "public",
+                table: "Project",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                schema: "public",
+                table: "User",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Username",
+                schema: "public",
+                table: "User",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Privacy",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "ProfessionalProfile",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Project",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "ProjectIssue",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "ProjectMember",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "ProjectTask",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "UserVerification",
+                schema: "public");
+        }
+    }
+}
